66e5e42c9e8b242618d718958b6b507df4fde244
Maurel ZODOGANHOU F3NOU ZM4 <maurelcath24@gmail.com>
2022-10-14 10:56:23 +0100

    Day 4 - Name display resolution code

---
+++ day4-nameDisplay.py
@@ -0,0 +1,8 @@
+firstName = str(input("Enter your first name:  "))
+lastName = str(input("Enter your last name : "))
+initials = firstName[0].title() + "." + lastName[0].upper()
+adress_book = firstName.title() + " " + lastName.upper()
+username = firstName[0].lower()+lastName.lower()   
+print("Initials  : " + initials)
+print("Adress book : " + adress_book)
+print("Username : " + username)
